a48568974ae3558226c7be362148f6b4133707f4
KoFE <petrovykh03@gmail.com>
2023-02-19 22:16:29 +0300

    First version

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+$('.destinations__slider').slick({
+    centerMode: true,
+    variableWidth: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    dots: true,
+    arrows: false
+});
