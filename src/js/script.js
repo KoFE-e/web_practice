e12f54cee2ba423109b73c15fe99129a95d241ba
KoFE <petrovykh03@gmail.com>
2023-02-20 23:19:53 +0300

    mobile adaptation

--- src/js/script.js
+++ src/js/script.js
@@ -5,5 +5,15 @@ $('.destinations__slider').slick({
     slidesToScroll: 1,
     infinite: true,
     dots: true,
-    arrows: false
+    arrows: false,
+    prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+    responsive: [
+        {
+            breakpoint: 576,
+            settings: {
+                arrows: true
+            }
+        }
+    ]
 });
